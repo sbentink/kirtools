5c05273ee227a2d0eaa2ba32735d049edf70d116
Stefan Bentink <bentink@dkms-lab.de>
2016-10-21 16:57:38 +0200

    Further development work. Added usage example in docuemtation of the PythonDB constructor.

--- R/kirallele.R
+++ R/kirallele.R
@@ -9,12 +9,12 @@ setGeneric(name="getKIRGeneNames",def=function(kil,...) {standardGeneric("getKIR
 
 #' Class \code{PythonDB}
 #'
-#' @description {S4 representing a KIR data base parsed from a Genebank file using a Python script.}
-#' @slot {pydb A \code{list_Python} object that hold the raw data obtained from a Genebank file.}
-#' @slot {locusNames A \code{character} vector with the KIR gene name of each allele in \code{pydb}}
-#' @slot {dbPath A \code{character} with a reference to the Genebank data file used to initialize the object}
-#' @slot {locusType \code{character} that defaults to 'KIR'. The python script could also handle locuses other than KIR.
-#' However we do not use this feature in this package.}
+#' @description S4 representing a KIR data base parsed from a Genebank file using a Python script.
+#' @slot pydb A \code{list_Python} object that hold the raw data obtained from a Genebank file.
+#' @slot locusNames A \code{character} vector with the KIR gene name of each allele in \code{pydb}
+#' @slot dbPath A \code{character} with a reference to the Genebank data file used to initialize the object
+#' @slot locusType \code{character} that defaults to 'KIR'. The python script could also handle locuses other than KIR.
+#' However we do not use this feature in this package.
 #' @importClassesFrom XRPython list_Python
 setClass("PythonDB",
          slots=
@@ -27,9 +27,9 @@ setClass("PythonDB",
 
 #' Class \code{KIRAllele}
 #'
-#' @description {A wrapper extending \code{HLAAllele-class} imported from \code{hlatools}.
-#' No methods or attributes in addition to the ones inherrited from \code{HLAAllele} are implemented in this implementation.
-#' This class should allows to handle KIR alleles in a separate object.}
+#' @description A wrapper extending \linkS4class{HLAAllele} imported from \link{hlatools}
+#' No methods or attributes in addition to the ones inherrited from \linkS4class{HLAAllele} are implemented in this implementation.
+#' This class should allow to handle KIR alleles in a separate object.
 #' @importClassesFrom hlatools HLAAllele
 setClass("KIRAllele",
          contains="HLAAllele")
@@ -45,7 +45,26 @@ setClass("KIRAlleleList",
                     data_source="character",
                     data_name="character"))
 
-PythonDB <- function(dbPath,locusType,gene,pythonPath) {
+#' Constructor for \linkS4class{PythonDB}
+#' @param Full path to KIR gene bank file
+#' @param locus type. The python script can handle more than just KIR data. However this package does not implement anything other than KIR alles.
+#' Hence this parameter defaults to 'KIR'. Don't change it.
+#' @param gene Giving a String with an actual gene name returns a \linkS4class{PythonDB} with a single entry that holds alleles for one gene. Setting this to 'all'
+#' will cause the parser to return a \linkS4class{PythonDB} that contains the entire data base
+#' @param pythonPath the path to the Python script (the core parser)
+#' @return A \linkS4class{PythonDB} object
+#' @examples
+#' \dontrun{
+#' dbPath=file.path("extdata","2016_10_05_KIR.dat")
+#' pythonPath="./exec"
+#' my_db_raw  <- PythonDB(dbPath,"KIR","all",pythonPath)
+#' my_db_list <- getAllelesFromPython(my_db_raw)
+#' my_db_list
+#' getKIRGene(kil=my_db_list,kirgene="KIR3DL2")
+#' getKIRGene(kil=my_db_list,kirgene=NA)
+#' }
+#' @export
+PythonDB <- function(dbPath,locusType="KIR",gene,pythonPath) {
             ev <- XRPython::RPython()
             XRPython::pythonAddToPath(pythonPath,evaluator = ev)
             ev$Import("hla_embl_parser","read_dat_file_simple_locus","read_dat_file_simple")
@@ -112,11 +131,13 @@ setMethod(f="getAlleleList",signature="dict_Python",
 }
 )
 
-#' Get list of \code{KIRAllele}s from a \code{PythonDB} object.
-#' @param {db A \code{PythonDB object from which we want to extract the allels}}
-#' @param {nccores The number of CPU cores that should be assigned this job. It is not huge computations carried out by this function,
-#' but parallel makes it a little bit faster anyway.}
-#' @return {A \code{list} of \code{KIRAllele} objects}
+
+#' @describeIn PythonDB Get list of \linkS4class{KIRAllele}s from a \linkS4class{PythonDB} object.
+#' @param db A \linkS4class{PythonDB} object from which we want to extract the alleles
+#' @param nccores The number of CPU cores that should be assigned this job. It is not huge computations carried out by this function,
+#' but parallel makes it a little bit faster anyway.
+#' @return A \linkS4class{KIRAlleleList}
+#' @export
 setMethod(f="getAllelesFromPython",signature="PythonDB",
           definition=function(db,ncores=parallel::detectCores()-4) {
 
@@ -144,7 +165,12 @@ setMethod(f="getAllelesFromPython",signature="PythonDB",
                                               locus=unlist(meta.raw[,4]))
             my.alleles.ind <- tapply(1:nrow(metaData),metaData$locus,function(i) i)
             my.alleles     <- parallel::mclapply(my.alleles.ind,KIRAllele,sequences,result,metaData,mc.cores=ncores)
-            my.alleles})
+            ka <- new("KIRAlleleList")
+            ka@alleles     <- my.alleles
+            ka@data_source <- "genebank"
+            ka@data_name   <- sapply(strsplit(db@dbPath,.Platform$file.sep),function(x) x[length(x)])
+            ka
+            })
 
 setMethod(f="show", signature="KIRAlleleList",definition=function(object) {
   kil=object
@@ -156,6 +182,7 @@ setMethod(f="show", signature="KIRAlleleList",definition=function(object) {
   print(lea)}
   )
 
+#' @export
 setMethod(f="getKIRGene",signature="KIRAlleleList",definition=function(kil,kirgene=NA) {
   if (is.na(kirgene)) return(kil@alleles[[1]])
   geneFound <- any(names(kil@alleles)==kirgene)
@@ -187,6 +214,12 @@ KIRAlleleList_gb <- function(
   ka
 }
 
+
+#' Extract alleles for specific KIR gene
+#' @description This function pulls all alleles from the data base file that are linked to one particular KIR gene.
+#' The alleles are returned as \linkS4class{KIRAllele} object
+#' @param genename The name of the KIR gene that should be pulled. Here it defaults to KIR2DL1.
+#' @return A \linkS4class{KIRAllele} object.
 #' @export
 KIRAlleleFromDB <- function(genename="KIR2DL1") {
   getKIRGene(KIRAlleleList_gb(kirGene = genename))
